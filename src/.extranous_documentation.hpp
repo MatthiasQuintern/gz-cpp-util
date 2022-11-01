5826df9afac90cb6081d38f4e8dbd377ccc3c945
matthias@arch <matthiasqui@protonmail.com>
2022-11-01 18:18:07 +0100

    improved documentation

--- src/.extranous_documentation.hpp
+++ src/.extranous_documentation.hpp
@@ -1,27 +1,23 @@
 namespace gz {
-    // deactivated to use readme as mainpage
-    /*
+    /**
      * @mainpage
      * @section main_about About
      *  This c++20 library contains multiple useful functions, most of them are focused on working with strings.
      *
      *  Please report bugs on the <a href="https://github.com/MatthiasQuintern/gz-cpp-util">github page</a>!
      *
+     *  For the changelog, have a look at the @ref README.md "readme".
+     *
      * @section main_features Features
-     *  @subsubsection main_f1 1. Logger
-     *   an @ref Log "extensive and extendable logger" using variadic templates to log almost anything
-     *  @subsubsection main_f2 2. Container
-     *   containers like a thread safe @ref Queue "queue" and a @ref RingBuffer "ringbuffer"
-     *  @subsubsection main_f3 3. Regex
-     *   regex that works with std::string_view
-     *  @subsubsection main_f4 4. String type conversion
-     *   - @ref sc_to_string "converting types to string" (including numbers, vectors, ranges, maps...)
-     *   - @ref sc_from_string "constructing types from string"
+     *  -# @ref Log "extensive and extendable logger" using variadic templates to log @ref sc_toStringImplemented "almost anything"
+     *  -# containers like a thread safe @ref Queue "queue" and a @ref RingBuffer "ringbuffer"
+     *  -# @ref regex.hpp "regex that works with std::string_view"
+     *  -# @subpage string_conversion "string <-> type conversion"
+     *   - @ref sc_toString "converting types to string" (including numbers, vectors, ranges, maps...)
+     *   - @ref sc_fromString "constructing types from string"
      *   - @ref getIntOr "constructing types from string" or return fallback
-     *  @subsubsection main_f5 5. Settings Manager
-     *   a @ref SettingsManager "settings manager" which can store settings of different types and load/save them from/to a file
-     *  @subsubsection main_f6 6. Enum-String generation script (python)
-     *   a script that generates an array of strings for your enumerations, so that you can get the name of an enum value
+     *  -# @ref SettingsManager "Settings Manager" which can store settings of different types and load/save them from/to a file
+     *  -# @subpage gen-enum-str "a script that generates an array of strings for your enumerations", so that you can get the name of an enum value
      *
      * @section main_installation Installation
      *  @subsection main_i_linux Linux
@@ -30,24 +26,10 @@ namespace gz {
      *  @subsection main_i_arch Arch Linux (ABS)
      *   - Download PKGBUILD: `wget https://raw.github.com/MatthiasQuintern/gz-cpp-util/main/PKGBUILD`
      *   - Build and install with the Arch Build System: `makepkg -si`
-     *  @subsection main_i_docs Documentation
-     *   The documentation for this library can be generated using **doxygen**. 
-     *   @code
-     *    cd gz-cpp-util/src
-     *    make docs
-     *    firefox ../docs/html/index.html
-     *   @endcode
-     *   Replace `firefox` with your web browser.
      *
-     * @section main_usage Usage
-     *  @subsection main_usage_lib Library
+     *  @subsection main_i_usage Usage
      *   - Add `-lgzutil` to your linker flags
      *   - Include the wanted header: `#include <gz-util/dir/header.hpp>`
-     *  
-     *  @subsection main_usage_p Enumeration-to-string script
-     *   - The gen_enum_str.py script will be installed to `/usr/bin/gz-enum-str`.
-     *   - Run `gz-enum-str -h` to list available options.
-     *
      *  
      */
 
@@ -77,7 +59,7 @@ namespace gz {
      * @section ges_usage Usage
      *  The script was written for python 3(.10), so you will need to have that installed.
      *
-     *  @subsubsection ges_usage_installed With script installed
+     *  @subsection ges_usage_installed With script installed
      *   Depending on the installation method, it should either be in `/usr/bin` or `/usr/local/bin` which should both be in your `$PATH`.
      *   Note that it will not have the `.py` ending!
      *
@@ -86,7 +68,7 @@ namespace gz {
      *    gen-enum-str ~/c++/src/myheader.hpp
      *   @endcode
      *
-     *  @subsubsection ges_usage_not_installed With script not installed
+     *  @subsection ges_usage_not_installed With script not installed
      *   If you do want to run the script on `~/c++/src/myheader.hpp`, with the script itself being located at `~/scripts/gen_enum_str.py`, run:
      *   @code shell
      *    python3 ~/scripts/gen_enum_str.py ~/c++/src/myheader.hpp
