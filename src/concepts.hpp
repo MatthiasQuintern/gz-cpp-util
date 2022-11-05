82385541a9b1f91f5b0afef0503ec75345766e05
matthias@arch <matthiasqui@protonmail.com>
2022-11-05 20:45:35 +0100

    Added range concepts

--- src/concepts.hpp
+++ src/concepts.hpp
@@ -14,6 +14,16 @@ namespace gz::util {
     /// Forward range holding integers
     template<typename T>
     concept IntegralForwardRange = std::ranges::forward_range<T> and std::integral<std::ranges::range_value_t<T>>;
+
+    /// Forward range holding ValueType
+	template<typename T, typename ValueType>
+	concept ForwardRange = std::ranges::forward_range<T> and std::same_as<std::ranges::range_value_t<T>, ValueType>;
+
+    /// Contiguous range holding ValueType
+	template<typename T, typename ValueType>
+	concept ContiguousRange = std::ranges::contiguous_range<T> and std::same_as<std::ranges::range_value_t<T>, ValueType>;
+
+ 
 }
 
 /**
